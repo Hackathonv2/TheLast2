05ddcb13136050135fdedb8cc8dd40b9a9f58f7d
Leandre17 <leandre.godet@epitech.eu>
2023-12-21 09:21:24 +0100

    [add] ex1

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -6,3 +6,14 @@ import sys
 lines = []
 for line in sys.stdin:
 	lines.append(line.rstrip('\n'))
+
+count = 0
+ingredients = []
+n = int(lines[0])
+for i in range(1, n+1):
+	t = lines[i].split()
+	for j in t:
+		if j not in ingredients:
+			count += 1
+			ingredients.append(j)
+print(count)
