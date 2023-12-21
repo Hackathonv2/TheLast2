a88064cda10f4217496fee5bdd650614a0629313
Leandre17 <leandre.godet@epitech.eu>
2023-12-21 10:16:31 +0100

    oui

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,8 +1,17 @@
-
-
-
 import sys
 
 lines = []
 for line in sys.stdin:
 	lines.append(line.rstrip('\n'))
+
+n, m = map(int, lines[0].split())
+pizz = [list(map(int, lines[i].split())) for i in range(1, n+1)]
+res = 0
+for order_i in lines[n+1: n+1+m]:
+    x, y = map(int, order_i.split())
+    best = float('inf')
+    for xp, yp in pizz:
+        dst = abs(xp-x) + abs(yp-y)
+        best = min(best, dst)
+    res += best
+print(2*res) # Account for all return trips
